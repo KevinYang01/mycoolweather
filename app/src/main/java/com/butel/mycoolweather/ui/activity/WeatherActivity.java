175a8c3511d7812038ea7eb0d759db69a3dc2e75
yanglin <1020678879@qq.com>
2018-05-29 14:52:43 +0800

    图片、文字等UI调整

--- app/src/main/java/com/butel/mycoolweather/ui/activity/WeatherActivity.java
+++ app/src/main/java/com/butel/mycoolweather/ui/activity/WeatherActivity.java
@@ -262,9 +262,9 @@ public class WeatherActivity extends AppCompatActivity {
             pm25Text.setText(weather.aqi.city.pm25);
         }
 
-        String comfort = "舒适度: " + weather.suggestion.comfort.info;
-        String carWash = "洗车指数 " + weather.suggestion.carWash.info;
-        String sport = "运动建议 " + weather.suggestion.sport.info;
+        String comfort = "舒适度：" + weather.suggestion.comfort.info;
+        String carWash = "洗车指数：" + weather.suggestion.carWash.info;
+        String sport = "运动建议：" + weather.suggestion.sport.info;
         comfortText.setText(comfort);
         carWashText.setText(carWash);
         sportText.setText(sport);
